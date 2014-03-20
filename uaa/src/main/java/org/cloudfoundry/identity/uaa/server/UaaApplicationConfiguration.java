abd062bd20a7784a88ece8c0032ce0919d0f62ca
Dave Syer <dsyer@gopivotal.com>
2014-03-20 15:10:26 +0000

    Extract some @Beans

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/server/UaaApplicationConfiguration.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/server/UaaApplicationConfiguration.java
@@ -1,13 +1,18 @@
 
 package org.cloudfoundry.identity.uaa.server;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.cloudfoundry.identity.uaa.UaaConfiguration;
 import org.cloudfoundry.identity.uaa.UaaConfiguration.UaaConfigConstructor;
+import org.cloudfoundry.identity.uaa.authentication.UaaAuthenticationDetailsSource;
+import org.cloudfoundry.identity.uaa.authentication.login.LoginInfoEndpoint;
 import org.cloudfoundry.identity.uaa.config.DataSourceConfiguration;
 import org.cloudfoundry.identity.uaa.config.YamlConfigurationValidator;
 import org.cloudfoundry.identity.uaa.security.web.SecurityFilterChainPostProcessor;
+import org.cloudfoundry.identity.uaa.web.HealthzEndpoint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
@@ -15,6 +20,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.AuthenticatedVoter;
+import org.springframework.security.access.vote.RoleVoter;
+import org.springframework.security.access.vote.UnanimousBased;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.error.OAuth2AuthenticationEntryPoint;
+import org.springframework.security.oauth2.provider.vote.ScopeVoter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 
 
 @Configuration
@@ -40,6 +56,71 @@ public class UaaApplicationConfiguration {
 		processor.setDumpRequests(dumpRequests);
 		processor.setRedirectToHttps(Arrays.asList("uiSecurity"));
 		return processor;
+	}
+	
+	@Bean
+	public UaaAuthenticationDetailsSource authenticationDetailsSource() {
+		return new UaaAuthenticationDetailsSource();
+	}
+	
+	@Bean
+	public OAuth2AuthenticationEntryPoint basicAuthenticationEntryPoint() {
+		OAuth2AuthenticationEntryPoint entry = new OAuth2AuthenticationEntryPoint();
+		entry.setRealmName("UAA/client");
+		entry.setTypeName("Basic");
+		return entry;
+	}
+	
+	@Bean
+	public OAuth2AuthenticationEntryPoint oauthAuthenticationEntryPoint() {
+		OAuth2AuthenticationEntryPoint entry = new OAuth2AuthenticationEntryPoint();
+		entry.setRealmName("UAA/oauth");
+		return entry;
+	}
+	
+	@Bean
+	public AccessDecisionManager accessDecisionManager() {
+		@SuppressWarnings("rawtypes")
+		List<AccessDecisionVoter> voters = new ArrayList<AccessDecisionVoter>();
+		ScopeVoter scopes = new ScopeVoter();
+		scopes.setScopePrefix("scope=");
+		voters.add(scopes);
+		voters.add(new RoleVoter());
+		voters.add(new AuthenticatedVoter());
+		return new UnanimousBased(voters);
+	}
+	
+	@Bean
+	public OAuth2AccessDeniedHandler oauthAccessDeniedHandler() {
+		return new OAuth2AccessDeniedHandler();
+	}
+	
+	@Bean
+	public Http403ForbiddenEntryPoint http403EntryPoint() {
+		return new Http403ForbiddenEntryPoint();
+	}
+	
+	@Bean
+	public SimpleUrlLogoutSuccessHandler logoutHandler() {
+		SimpleUrlLogoutSuccessHandler handler = new SimpleUrlLogoutSuccessHandler();
+		handler.setDefaultTargetUrl("/");
+		handler.setTargetUrlParameter("redirect");
+		return handler;
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bcryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public LoginInfoEndpoint loginInfoEndpoint() {
+		return new LoginInfoEndpoint();
+	}
+	
+	@Bean
+	public HealthzEndpoint healthzEndpoint() {
+		return new HealthzEndpoint();
 	}
 
 }
