56164b768af8fe5c398aab6c818b0a7b94dfca2f
Dave Syer <dsyer@gopivotal.com>
2014-03-20 14:22:20 +0000

    Add @EnableAutoConfiguration

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/server/UaaApplicationConfiguration.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/server/UaaApplicationConfiguration.java
@@ -1,13 +1,45 @@
+
 package org.cloudfoundry.identity.uaa.server;
 
+import java.util.Arrays;
+
+import org.cloudfoundry.identity.uaa.UaaConfiguration;
+import org.cloudfoundry.identity.uaa.UaaConfiguration.UaaConfigConstructor;
 import org.cloudfoundry.identity.uaa.config.DataSourceConfiguration;
+import org.cloudfoundry.identity.uaa.config.YamlConfigurationValidator;
+import org.cloudfoundry.identity.uaa.security.web.SecurityFilterChainPostProcessor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
+
 @Configuration
 @Import(DataSourceConfiguration.class)
 @ImportResource("classpath:/spring-servlet.xml")
+@EnableAutoConfiguration(exclude=SecurityAutoConfiguration.class)
 public class UaaApplicationConfiguration {
+
+	@Bean
+	protected YamlConfigurationValidator<UaaConfiguration> yamlConfigurationValidator(
+			@Value("${environmentYamlKey}") String yaml) {
+		YamlConfigurationValidator<UaaConfiguration> validator = new YamlConfigurationValidator<UaaConfiguration>(
+				new UaaConfigConstructor());
+		validator.setYaml(yaml);
+		return validator;
+	}
+
+	@Bean
+	protected SecurityFilterChainPostProcessor securityFilterChainPostProcessor(
+			@Value("${require_https:false}") boolean requireHttps, @Value("${dump_requests:false}") boolean dumpRequests) {
+		SecurityFilterChainPostProcessor processor = new SecurityFilterChainPostProcessor();
+		processor.setRequireHttps(requireHttps);
+		processor.setDumpRequests(dumpRequests);
+		processor.setRedirectToHttps(Arrays.asList("uiSecurity"));
+		return processor;
+	}
 
 }
